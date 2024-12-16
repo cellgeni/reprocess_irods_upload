f249d43a4d29e1a30ee1ce2d058c9772e502eec4
Aljes <aleksandr.binkevich@gmail.com>
2024-12-16 17:26:22 +0000

    Ensure correct number of arguments

--- scripts/submit_process.sh
+++ scripts/submit_process.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+
+# Ensure correct number of arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <metadata_directory> <irods_target_directory>"
+  exit 1
+fi
+
 INPUT_FILE=$1
 SCRIPT=/lustre/scratch127/cellgen/cellgeni/aljes/reprocess/scripts/reprocess.bsub
 : > finished.tsv
