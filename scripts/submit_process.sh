62c2c9418a8adf7505100ba96610281d7ae1e34d
Aljes <aleksandr.binkevich@gmail.com>
2025-01-07 13:06:41 +0000

    Check if dataset directory exists and subset file is not empty before submiting the job

--- scripts/submit_process.sh
+++ scripts/submit_process.sh
@@ -3,7 +3,7 @@
 
 # Ensure correct number of arguments
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <metadata_directory> <irods_target_directory>"
+  echo "Usage: $0 <list_of_datasets>"
   exit 1
 fi
 
@@ -12,7 +12,14 @@ SCRIPT=/lustre/scratch127/cellgen/cellgeni/aljes/reprocess/scripts/reprocess.bsu
 : > finished.tsv
 
 while IFS=$'\t' read -r DATASET SPECIE NUM_ALL NUM_10X SAMPLES
-do  
-    echo $SAMPLES | tr ',' '\n' > "${DATASET}_subset.txt"
+do
+  # write subset list to file
+  echo $SAMPLES | tr ',' '\n' > "${DATASET}_subset.txt"
+  
+  if [[ ! -d $DATASET && -s "${DATASET}_subset.txt" ]]
+  then
     bsub -env "all, DATASET=$DATASET, SUBSET=${DATASET}_subset.txt" < $SCRIPT
+  else
+    echo "Directory $DATASET exists or subset file is empty. Skipping..."
+  fi
 done < "$INPUT_FILE"
