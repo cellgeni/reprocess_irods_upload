3a76f00b4be96a9fa796aec6f595c8760a7e284b
Aljes <aleksandr.binkevich@gmail.com>
2025-01-07 16:21:23 +0000

    Added output file as script's argument

--- scripts/count_success_logs.sh
+++ scripts/count_success_logs.sh
@@ -2,12 +2,13 @@
 
 pattern="Successfully completed."
 directory=$1
+output=$2
 
 total_logs=$(find $directory -type f -name "output*.log" | wc -l)
 successful_logs=$(find $directory -type f -name "output*.log" -exec grep -q "$pattern" {} \; -print | wc -l)
 
-find "$directory" -type f -name "output*.log" ! -exec grep -q "$pattern" {} \; -print > failed_logs.txt
+find "$directory" -type f -name "output*.log" ! -exec grep -q "$pattern" {} \; -print > $output
 
 echo "Total output*.log files: $total_logs"
 echo "Files containing 'Successfully completed.': $successful_logs"
-echo "Files do not contain 'Successfully completed.': $( wc -l failed_logs.txt )"
+echo "Files do not contain 'Successfully completed.': $( wc -l $output )"
